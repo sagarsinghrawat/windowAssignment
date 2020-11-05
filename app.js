9ce8fd609adfaab5ab1974428a5ba6e0335eea85
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-11-05 14:43:50 +0530

    My Assignment

---
+++ app.js
@@ -0,0 +1,15 @@
+
+$("#div1").accordion({
+            collapsible: true,
+            active: false,
+            animate: 1000,
+            heightStyle: true,
+            icons: {header: "ui-icon-plus", activeHeader: "ui-icon-minus"}
+        });
+
+$(".img1").tooltip({
+            //title: "C++ Programming language",
+    track: true,
+    show :{ effect:"bounce", duration: 3000},
+    //hide :{ effect:"explode", duration: 3000}
+});
