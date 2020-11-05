5912320016d1e6698b1abc0bf0f9ce5918659233
sagarsinghrawat <sagarrawatssr9647@gmail.com>
2020-11-06 01:44:55 +0530

    image changes

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+$(document).ready( function(){
 
-$("#div1").accordion({
-            collapsible: true,
-            active: false,
-            animate: 1000,
-            heightStyle: true,
-            icons: {header: "ui-icon-plus", activeHeader: "ui-icon-minus"}
-        });
+    $("#div1").accordion({
+        collapsible: true,
+        active: false,
+        animate: 1000,
+        heightStyle: true,
+        icons: {header: "ui-icon-plus", activeHeader: "ui-icon-minus"}
+    });
 
-$(".img1").tooltip({
-            //title: "C++ Programming language",
-    track: true,
-    show :{ effect:"bounce", duration: 3000},
-    //hide :{ effect:"explode", duration: 3000}
-});
+    $(".img1").tooltip({
+        //title: "C++ Programming language",
+        track: true,
+        show :{ effect:"bounce", duration: 2000},
+        //hide :{ effect:"explode", duration: 3000}
+    });
+})
